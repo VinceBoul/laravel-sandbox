e562d68f3cb2b8292cf8a73075e0296bb8085a64
Vincent <toullec.0104@apside-groupe.com>
2020-11-25 10:47:52 +0100

    flight entity

---
+++ resources/views/flight/list.blade.php
@@ -0,0 +1,19 @@
+<x-jet-action-section>
+    <x-slot name="title">
+        {{ __('Two Factor Authentication') }}
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Add additional security to your account using two factor authentication.') }}
+    </x-slot>
+
+    <x-slot name="content">
+
+        <div class="mt-3 max-w-xl text-sm text-gray-600">
+            <p>
+                {{ __('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.') }}
+            </p>
+        </div>
+
+    </x-slot>
+</x-jet-action-section>
